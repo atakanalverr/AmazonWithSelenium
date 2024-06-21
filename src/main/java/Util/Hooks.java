ccfa832711ca6540c156a144a3682323278c56d2
atakanalver <atakan.alverr@gmail.com>
2024-06-21 12:49:29 +0300

    AmazonWithSelenium

--- src/main/java/Util/Hooks.java
+++ src/main/java/Util/Hooks.java
@@ -1,20 +1,30 @@
 package Util;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import io.cucumber.java.*;
 import org.openqa.selenium.WebDriver;
 import org.testng.Reporter;
+
 import java.util.Properties;
 
 public class Hooks {
+
     WebDriver driver;
     Properties properties;
     @Before
     public void before() {
         String browser = Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("browser");
-        properties = ConfigReader.initialize_Properties();
-        driver = DriverFactory.initialize_Driver(browser);
+        properties = Util.ConfigReader.initialize_Properties();
+        driver = Util.DriverFactory.initialize_Driver(browser);
     }
+
+    @BeforeStep
+    public void beforeStep() {
+    }
+
+    @AfterStep
+    public void afterStep() {
+    }
+
     @After
     public void after() {
         driver.quit();
